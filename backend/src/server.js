71e954a818d18e30efd70c0b441429a60f7e08c3
atchiang <156946894+atchiang@users.noreply.github.com>
2025-10-01 18:34:05 -0700

    Initialization + routes + controllers

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+// const express = require ("express");
+import notesRoutes from "./routes/notesRoutes.js"
+
+const app = express();
+
+app.use("/api/notes", notesRoutes);
+
+// why do we need an api? cannot just directly interact with database
+// Endpoint is a combination of URL + HTTP method that lets client 
+// interact with specific resources
+
+
+app.listen(5001, () => {
+    console.log("Server started on PORT: 5001");
+});
+
